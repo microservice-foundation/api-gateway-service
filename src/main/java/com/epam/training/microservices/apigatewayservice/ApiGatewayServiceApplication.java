23f9375dd5552f10b205020d223505b4b8ddc7e9
Yusuf Murodov <yusuf_murodov@epam.com>
2023-09-04 11:34:46 +0500

    Add context propogation

--- src/main/java/com/epam/training/microservices/apigatewayservice/ApiGatewayServiceApplication.java
+++ src/main/java/com/epam/training/microservices/apigatewayservice/ApiGatewayServiceApplication.java
@@ -3,11 +3,13 @@ package com.epam.training.microservices.apigatewayservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import reactor.core.publisher.Hooks;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ApiGatewayServiceApplication {
     public static void main(String[] args) {
+        Hooks.enableAutomaticContextPropagation();
         SpringApplication.run(ApiGatewayServiceApplication.class, args);
     }
 }
